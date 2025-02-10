7f2bed6a545cde0e78f695cfc9627ee2c370efd9
Khoa Vo <anhkhoavo2601@gmail.com>
2025-02-10 19:42:05 +0100

    firebase deploy - Broken ling

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,58 +1,41 @@
 import React from 'react';
 import Layout from '@theme/Layout'; // Import Layout từ Docusaurus
 import PureHero from '@site/src/components/PureHero/PureHero';
-import TwoColumnSection from '@site/src/components/TwoColumnSection/TwoColumnSection';
+import FAQSection from '@site/src/components/FAQSection/FAQSection';
 
+const faqs = [
+    { 
+        question: 'Nếu tôi có thắc mắc hoặc cần giúp đỡ, tôi liên hệ ai?', 
+        answer: 'Bạn có thể tham gia vào cộng đồng `Deutsch.vn` – nơi không chỉ có những người học như bạn mà còn có các thầy cô dạy tiếng Đức và những người bạn với trình độ vững chắc. Tại đây, mọi người luôn sẵn lòng chia sẻ kinh nghiệm và giúp bạn giải đáp mọi thắc mắc trong hành trình chinh phục tiếng Đức.' 
+    },
+    { 
+        question: 'Liệu bạn có đang sử dụng cộng đồng để quảng cáo hay thu lợi không?', 
+        answer: 'Mục tiêu của tôi là xây dựng một không gian học tập thân thiện, không bị chi phối bởi quảng cáo.' 
+    },
+    { 
+        question: 'Đây có thật sự là trang web miễn phí không?', 
+        answer: 'Hoàn toàn miễn phí! Với hơn 5 năm kinh nghiệm xây dựng nhiều cộng đồng chất lượng, mình hiểu rõ giá trị của việc chia sẻ kiến thức. Trang web này đơn thuần chỉ là nơi mình công khai những tài liệu học tập cá nhân mà mình đã tích lũy và sử dụng trong quá trình học tiếng Đức. Vì vậy, bạn có thể yên tâm rằng đây chắc chắn là một trang web miễn phí.' 
+    }
+];
 
 const HomePage = () => {
     return (
         <Layout title="Trang chủ" description="Học hỏi không giới hạn">
-            <div>
-                {/* Hero Section */}
-                <PureHero 
-                    title="Cùng học tiếng Đức với mình nhé!" 
-                    subtitle="Bạn không cần phải học thật nhanh, chỉ cần học theo cách phù hợp với chính mình. Và đây là tất cả tài liệu mình ghi chú lại trong quá trình học của mình. Mong là sẽ giúp được bạn phần nào đó!" 
-                    backgroundImage="https://images.unsplash.com/photo-1453219562534-36e2ce0ea18e?q=80&w=1973&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                    buttonText="Tham gia cộng đồng"
-                    buttonLink="#" 
-                    contentWidth="60%"
-                    contentPosition="center"
-                    titleColor="#ffffff"
-                    subtitleColor="#f0f0f0"
-                    buttonColor="#ffffff"
-                    navColor="#ffffff"
-                />
-
-                {/* Section 1 */}
-                <TwoColumnSection
-                  title="Câu Chuyện Của Chúng Tôi"
-                  description="Learn Anything luôn khuyến khích bạn giống như nước – không ngừng tìm kiếm, thay đổi và thích nghi với những thử thách mới. Học tập là con đường rộng mở, nơi bạn không ngừng hoàn thiện bản thân mỗi ngày. Đây là nơi bạn không chỉ học để biết, mà còn học để tìm thấy phiên bản linh hoạt, mạnh mẽ và không ngừng thay đổi của chính mình."
-                  buttonText="Kết nối"
-                  buttonLink="#"
-                  titleColor="#000000"
-                  descriptionColor="#333333"
-                  buttonColor="#000000"
-                  imageSrc="https://images.unsplash.com/photo-1502680390469-be75c86b636f?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                  imageAlt="Placeholder Image"
-                  imagePosition="right"
-                  columnRatio='1.5'
-                />
-
-                {/* Section 2 */}
-                <TwoColumnSection
-                  title="Cùng học tiếng Đức với mình nhé!"
-                  description="Bạn không cần phải học thật nhanh, chỉ cần học theo cách phù hợp với chính mình. Và đây là tất cả tài liệu mình ghi chú lại trong quá trình học của mình. Mong là sẽ giúp được bạn phần nào đó!"
-                  // buttonText="Tìm hiểu"
-                  // buttonLink="#"
-                  titleColor="#000000"
-                  descriptionColor="#333333"
-                  buttonColor="#000000"
-                  imageSrc="https://images.unsplash.com/photo-1494783367193-149034c05e8f?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                  imageAlt="Placeholder Image"
-                  imagePosition="left"
-                  columnRatio='1.5'
-                />
-            </div>
+            <PureHero 
+                title="Cùng học tiếng Đức với mình nhé!" 
+                subtitle="Bạn không cần phải học thật nhanh, chỉ cần học theo cách phù hợp với chính mình. Và đây là tất cả tài liệu mình ghi chú lại trong quá trình học của mình. Mong là sẽ giúp được bạn phần nào đó!" 
+                backgroundImage="https://images.unsplash.com/photo-1453219562534-36e2ce0ea18e?q=80&w=1973&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                buttonText="Tham gia cộng đồng"
+                buttonLink="#" 
+                contentWidth="60%"
+                contentPosition="center"
+                titleColor="#ffffff"
+                subtitleColor="#f0f0f0"
+                buttonColor="#ffffff"
+                navColor="#ffffff"
+            />
+            
+            <FAQSection faqs={faqs} />
         </Layout>
     );
 };
